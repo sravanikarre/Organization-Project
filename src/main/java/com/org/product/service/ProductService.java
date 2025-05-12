ea6dff7d810cb8fb11721e81640a97e248e0b243
sravani.karre <sravani.kj109@gmail.com>
2025-05-12 10:04:55 -0500

    Changes made to org_changes2

--- src/main/java/com/org/product/service/ProductService.java
+++ src/main/java/com/org/product/service/ProductService.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 public interface ProductService {
 
- //  Product saveProduct(Product product);
+  void saveProduct(Product product);
 
     List<Product> getAllProducts();
 //
